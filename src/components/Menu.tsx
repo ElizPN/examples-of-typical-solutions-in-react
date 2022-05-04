fdacaaf1b1bf5e8c8ee872761d0d617b2a7127b7
ElizPN <pankevych.liza@gmail.com>
2022-05-04 18:20:43 +0200

    add mui buttons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@mui/material";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 
 export const Menu: FC = () => {
   return (
     <>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <Button to='/' variant='outlined' component={Link}>
+        Home
+      </Button>
+
+      <Button to='/about' variant='outlined' component={Link}>
+        About
+      </Button>
     </>
   );
 };
