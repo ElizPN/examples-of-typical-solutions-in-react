5750a5a7f1ef84600f38aa6d510420e2c921e14a
ElizPN <pankevych.liza@gmail.com>
2022-05-06 10:26:04 +0200

    add FeaturedPost component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./general.css";
 import { Menu } from "./components/Menu";
@@ -10,11 +10,32 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import { Header } from "./components/Header";
+import { FeaturedPost } from "./components/FeaturedPost";
 
 const sections = [
   { title: "Arrays", url: "#" },
   { title: "Array's objects", url: "#" },
   { title: "Components", url: "#" },
+];
+// const mainFeaturedPost
+
+const featuredPosts = [
+  {
+    title: "Featured post",
+    date: "Nov 12",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageLabel: "Image Text",
+  },
+  {
+    title: "Post title",
+    date: "Nov 11",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageLabel: "Image Text",
+  },
 ];
 
 const theme = createTheme();
@@ -26,6 +47,13 @@ export default function App() {
       <BrowserRouter>
         <Container maxWidth='lg'>
           <Header title='Blog' sections={sections} />
+          <main>
+            <Grid container spacing={4}>
+              {featuredPosts.map((post) => (
+                <FeaturedPost key={post.title} post={post} />
+              ))}
+            </Grid>
+          </main>
 
           <h1>Welcome! </h1>
           <Menu />
