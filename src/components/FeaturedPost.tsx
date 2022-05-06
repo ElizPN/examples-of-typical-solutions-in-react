339f08f141b5dbc76eef4b474c5d9c3fdead76b1
ElizPN <pankevych.liza@gmail.com>
2022-05-06 10:54:19 +0200

    add Typography

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Grid";
 import CardActionArea from "@mui/material/CardActionArea";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 interface FeaturedPostProps {
   post: {
@@ -21,7 +22,17 @@ export function FeaturedPost(props: FeaturedPostProps) {
     <Grid item xs={12} md={6}>
       <CardActionArea component='a' href='#'>
         <Card sx={{ display: "flex" }}>
-          <CardContent sx={{ flex: 1 }}></CardContent>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component='h2' variant='h5'>
+              {post.title}
+            </Typography>
+            <Typography variant='subtitle1' color='text.secondary'>
+              {post.date}
+            </Typography>
+            <Typography variant='subtitle1' color='primary'>
+              Continue reading...
+            </Typography>
+          </CardContent>
         </Card>
       </CardActionArea>
     </Grid>
