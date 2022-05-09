ca450750223bcf7db1b5331ba80eaf2a995cb587
ElizPN <pankevych.liza@gmail.com>
2022-05-09 14:02:40 +0200

    get prop (state)  from Page component.

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
+interface BodyProps {
+  bodyState: string;
+}
 
-var contentful = require("contentful");
-
-const client = contentful.createClient({
-  space: "a4alnltpsauh",
-  accessToken: "n_8u2e0aBdG_U7V2o6u1ClYynSy1MZeblGnrCXOuWhQ",
-});
-
-export function Body() {
-  const [bodyState, setBodyState] = useState("");
-
-  client.getEntry("4KzAcNlgeLWb7WynZwVXDl").then((entry: any) => {
-    setBodyState(entry.fields.body.content[0].content[0].value);
-
-    console.log(bodyState);
-  });
+export function Body(props: BodyProps) {
+  const { bodyState } = props;
 
   return <p>{bodyState}</p>;
 }
