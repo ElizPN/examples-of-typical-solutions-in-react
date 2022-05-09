d51e3c89a9d80691dc46885364d8e7b3d32ef3cf
ElizPN <pankevych.liza@gmail.com>
2022-05-09 17:52:37 +0200

    move entryId to smart PageRouter component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,8 @@
 import Box from "@mui/material/Box";
 import { useState } from "react";
 import { Body } from "./Body";
+import { Counter } from "./Counter";
+
 import { Title } from "./Title";
 
 var contentful = require("contentful");
@@ -10,11 +12,17 @@ const client = contentful.createClient({
   accessToken: "n_8u2e0aBdG_U7V2o6u1ClYynSy1MZeblGnrCXOuWhQ",
 });
 
-export function Page() {
+interface PageProps {
+  entryId: string;
+}
+
+export function Page(props: PageProps) {
+  const { entryId } = props;
+
   const [titleState, setTitleState] = useState("");
   const [bodyState, setBodyState] = useState("");
 
-  client.getEntry("4KzAcNlgeLWb7WynZwVXDl").then((entry: any) => {
+  client.getEntry(entryId).then((entry: any) => {
     setTitleState(entry.fields.title);
     setBodyState(entry.fields.body.content[0].content[0].value);
   });
@@ -22,6 +30,7 @@ export function Page() {
   return (
     <Box>
       <Title titleState={titleState} />
+      <Counter />
       <Body bodyState={bodyState} />
     </Box>
   );
