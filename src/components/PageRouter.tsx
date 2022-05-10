42f0b3b88318e8d20d861bc70367e2202b017474
ElizPN <pankevych.liza@gmail.com>
2022-05-10 13:09:22 +0200

    create confg object

--- src/components/PageRouter.tsx
+++ src/components/PageRouter.tsx
@@ -1,28 +1,34 @@
 import Box from "@mui/material/Box";
 import { useState } from "react";
+import { contentfulConfig } from "../config/contentfulConfig";
 import { Counter } from "./Counter";
 import { Page } from "./Page";
 
 var contentful = require("contentful");
 
+const configPage = {
+  entryId: "4KzAcNlgeLWb7WynZwVXDl",
+  componentExample: Counter,
+};
+
 export function PageRouter() {
-  const client = contentful.createClient({
-    space: "a4alnltpsauh",
-    accessToken: "n_8u2e0aBdG_U7V2o6u1ClYynSy1MZeblGnrCXOuWhQ",
-  });
-  const entryId = "4KzAcNlgeLWb7WynZwVXDl";
+  const client = contentful.createClient(contentfulConfig);
 
   const [titleState, setTitleState] = useState("");
   const [bodyState, setBodyState] = useState("");
 
-  client.getEntry(entryId).then((entry: any) => {
+  client.getEntry(configPage.entryId).then((entry: any) => {
     setTitleState(entry.fields.title);
     setBodyState(entry.fields.body.content[0].content[0].value);
   });
 
   return (
     <Box>
-      <Page TemplatePage={Counter} title={titleState} body={bodyState} />
+      <Page
+        ComponentExampe={configPage.componentExample}
+        title={titleState}
+        body={bodyState}
+      />
     </Box>
   );
 }
