e455f33fdccb26b8503add72ed29139fa01c2661
ElizPN <pankevych.liza@gmail.com>
2022-05-30 17:29:51 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ import Link from "@mui/material/Link";
 function Copyright() {
   return (
     <Typography variant='body2' color='text.secondary' align='center'>
-      {"Copyright © "}
+      {"© "}
       <Link color='inherit' href='https://github.com/ElizPN'>
         My GitHub
       </Link>{" "}
@@ -18,28 +18,17 @@ function Copyright() {
   );
 }
 
-interface FooterProps {
-  description: string;
-  title: string;
-}
-
-export default function Footer(props: FooterProps) {
-  const { description, title } = props;
-
+export default function Footer() {
   return (
     <Box component='footer' sx={{ bgcolor: "background.paper", py: 6 }}>
       <Container maxWidth='lg'>
-        <Typography variant='h6' align='center' gutterBottom>
-          {title}
-        </Typography>
+        <Typography variant='h6' align='center' gutterBottom></Typography>
         <Typography
           variant='subtitle1'
           align='center'
           color='text.secondary'
           component='p'
-        >
-          {description}
-        </Typography>
+        ></Typography>
         <Copyright />
       </Container>
     </Box>
